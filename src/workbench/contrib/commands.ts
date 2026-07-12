38886c2426adda635c8aea0bbece296faac438d4
haitao2016 <renshenchujiu@126.com>
2026-07-12 20:24:19 +0800

    fix: correct import path in commands.ts

--- src/workbench/contrib/commands.ts
+++ src/workbench/contrib/commands.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { DisposableStore, IWorkbenchContribution } from '../../common';
+import { DisposableStore, IWorkbenchContribution } from '../common';
 
 interface ICommand { id: string; handler: (...args: unknown[]) => unknown; }
 
@@ -9,7 +9,4 @@ export function registerCommand(id: string, handler: (...args: unknown[]) => unk
   _commands.add(vscode.commands.registerCommand(id, handler));
 }
 
-export const Commands = {
-  register: registerCommand,
-  store: _commands,
-};
+export const Commands = { register: registerCommand, store: _commands };
